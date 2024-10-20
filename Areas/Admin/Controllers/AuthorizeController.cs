87bb891afeeba6b1f28bfdd0eb0790bf7c61d0e5
pvquang89 <phamvanquang12042002@gmail.com>
2024-10-20 23:18:37 +0700

    Create AuthorizeController, add index view

---
+++ Areas/Admin/Controllers/AuthorizeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebPhongKham.Extension;
+using WebPhongKham.Models;
+
+namespace WebPhongKham.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //chỉ mangager mới dùng được chức năng này
+    [SessionAuthorize("Manager")] 
+    public class AuthorizeController : Controller
+    {
+        public readonly AppDbContext _context;
+        public AuthorizeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var listAcc = _context.Accounts.Include(a => a.AccountRoles)
+                                            .ThenInclude(ar=>ar.Role)
+                                            .ToList();
+            return View(listAcc);
+        }
+    }
+}
