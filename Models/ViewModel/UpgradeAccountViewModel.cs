5e777634568eebef75c291db6958199847b355fb
pvquang89 <phamvanquang12042002@gmail.com>
2025-03-28 14:59:58 +0700

    [Feature] vnpay payment integration

---
+++ Models/ViewModel/UpgradeAccountViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPhongKham.Models.ViewModel
+{
+    public class UpgradeAccountViewModel
+    {
+        public string PackageName { get; set; }
+        public long Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/ViewModel/VnPaymentResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebPhongKham.Models.ViewModel
+{
+    public class VnPaymentResponseModel
+    {
+        public bool Success { get; set; }
+        public string PaymentMethod { get; set; }
+        public string OrderDescription { get; set; }
+        public string OrderId { get; set; }
+        public string PaymentId { get; set; }
+        public string TransactionId { get; set; }
+        public string Token { get; set; }
+        public string VnPayResponseCode { get; set; }
+
+    }
+
+    public class VnPaymentRequest
+    {
+        public int OrderId { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
